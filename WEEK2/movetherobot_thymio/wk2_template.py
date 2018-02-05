5e3c0594ea9474c30a7bb54e07585eee6b4b2bba
TAYTS <taytzushieh@gmail.com>
2018-02-05 12:31:50 +0800

    First Commit

---
+++ WEEK2/movetherobot_thymio/wk2_template.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pythymiodw import *
+
+def print_temp(t_celcius):
+    t_F = t_celcius * 9/5 + 32
+    print("The temperature in Celsius is {:.3f} and Fahrenheit is {:.3f}".format(float(t_celcius), float(t_F))
+
+def forward(speed, duration):
+    robot.wheels(speed, speed)
+    robot.sleep(duration) 
+
+robot = ThymioReal() # create an object
+
+############### Start writing your code here ################ 
+
+# Prompt user to enter speed and duration of movement
+speed = int(input("Please enter the speed: "))
+print(str(speed))
+duration = int(input("Please enter the duration: "))
+print(str(duration))
+
+# Move according to the specified speed and duration
+forward(speed, duration)
+
+# Read temperature in celcius from the sensor and print it
+print_temp(robot.get_temperature())
+
+########################## end ############################## 
+
+robot.quit() # disconnect the communication
+
