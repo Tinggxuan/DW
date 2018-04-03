2b91fd8fef6b9788c8322a04319afc1d30e8c795
TAYTS <taytzushieh@gmail.com>
2018-04-03 21:09:35 +0800

    Change the led name from 'yellow' to 'green'

--- WEEK11/guiwithkivy/pi_template.py
+++ WEEK11/guiwithkivy/pi_template.py
@@ -8,7 +8,7 @@ url="https://dw-1d-2018.firebaseio.com"
 firebase=firebase.FirebaseApplication(url,token)
 
 GPIO.setmode(GPIO.BCM)
-ledcolor={'yellow':23, 'red':24}
+ledcolor={'green':23, 'red':24}
 
 GPIO.setup(ledcolor.values(), GPIO.OUT)
 
