b30d38d26038bae358dcb01cb3f64d8ac496bd15
TAYTS <taytzushieh@gmail.com>
2018-04-05 09:20:27 +0800

    Add abstract class function for class 'Tank'

--- test_module/tank/tank.py
+++ test_module/tank/tank.py
@@ -1,2 +1,15 @@
 class Tank:
-    pass
+    
+    def __init__(self):
+        pass
+
+
+    def water_lvl(self):
+        pass
+
+    def valve(self):
+        pass
+
+    def pump(self):
+        pass
+
