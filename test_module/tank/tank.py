1cc89ea8955414b8a4e7f1c1e917e0f11a61b366
TAYTS <taytzushieh@gmail.com>
2018-04-05 20:52:48 +0800

    save all the child class obj using dictionary for easy reference later

--- test_module/tank/tank.py
+++ test_module/tank/tank.py
@@ -9,14 +9,20 @@ class Tank:
         self.pump = self.pump(kwargs['pump'])
 
     def water_sensor(self, pin_num): # create water sensor obj and add it to Tank obj
-        tank_water_sensor = Water_Sensor(pin_num)
+        tank_water_sensor = {}
+        for key, value in pin_num.items():
+            tank_water_sensor[key] = Water_Sensor(value)
         return tank_water_sensor
 
     def valve(self, pin_num): # create valve obj and add it to Tank obj
-        tank_valve = Value(pin_num)
+        tank_valve = {}
+        for key, value in pin_num.items():
+            tank_valve[key] = Valve(value)
         return tank_valve
 
     def pump(self, pin_num): # create pump obj and add it to Tank obj
-        tank_pump = Pump(pin_num)
+        tank_pump = {}
+        for key, value in pin_num.items():
+            tank_pump[key] = Pump(value)
         return tank_pump
 
