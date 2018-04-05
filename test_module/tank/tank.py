68b83f45b670d07d1b8e379f52952510d04d65f1
TAYTS <taytzushieh@gmail.com>
2018-04-05 10:48:52 +0800

    Add function call to __init__

--- test_module/tank/tank.py
+++ test_module/tank/tank.py
@@ -1,15 +1,21 @@
 class Tank:
     
-    def __init__(self):
+    def __init__(self, kwargs):
+        # digital pin for water lvl sensor
+        self._water_lvl = self.water_lvl()
+        # call valve to add the valve
+        self._valve = self.valve()
+        # call pump to add the pump
+        self._pump = self.pump()
+
+    def water_lvl(self): # receive digital pin and get the water lvl
         pass
+        
 
-
-    def water_lvl(self):
+    def valve(self): # create valve obj and add it to Tank obj
         pass
+        
 
-    def valve(self):
+    def pump(self): # create pump obj and add it to Tank obj
         pass
-
-    def pump(self):
-        pass
-
+        
