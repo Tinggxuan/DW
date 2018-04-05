fde40a3b0551c08853049345d85c9b902d4c5853
TAYTS <taytzushieh@gmail.com>
2018-04-05 20:31:34 +0800

    Assign the 'pin_num' to each child class of Tank using keywords

--- test_module/tank/tank.py
+++ test_module/tank/tank.py
@@ -2,11 +2,11 @@ class Tank:
     
     def __init__(self, kwargs):
         # digital pin for water lvl sensor
-        self.water_sensor = self.water_sensor()
+        self.water_sensor = self.water_sensor(kwargs['water_sensor'])
         # call valve to add the valve
-        self.valve = self.valve()
+        self.valve = self.valve(kwargs['valve'])
         # call pump to add the pump
-        self.pump = self.pump()
+        self.pump = self.pump(kwargs['pump'])
 
     def water_sensor(self, pin_num): # create water sensor obj and add it to Tank obj
         tank_water_sensor = Water_Sensor(pin_num)
