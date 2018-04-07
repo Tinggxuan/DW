1e48520a3c1f347fd8e0bdc6736680a9e8387a0d
TAYTS <taytzushieh@gmail.com>
2018-04-07 23:14:26 +0800

    Test the main function and 'Tank' class object creation

--- test_module/project_class/main.py
+++ test_module/project_class/main.py
@@ -1,6 +1,7 @@
 import time
 import ADS1x15
 import RPi.GPIO as GPIO
+import Tank
 
 
 # create adc object to read the analog reading
@@ -8,9 +9,17 @@ adc1 = ADS1x15.ADS1115(address=0x48)
 adc2 = ADS1x15.ADS1115(address=0x49)
 
 
+rain_water_tank = Tank({'water_sensor':{'low':0},\
+                        'valve':{'in':20,'out':21},\
+                        'pump':{'in':19,'out':26}},adc1)
+
 try:
     while True:
-        pass
+        if rain_water_tank.water_sensor['low'].get_status() == True:
+            rain_water_tank.valve['in'].activate(5)
+            rain_water_tank.valve['out'].activate(5)
+            rain_water_tank.pump['in'].activate(5)
+            rain_water_tank.pump['out'].activate(5)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
