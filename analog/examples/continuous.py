2d71485b316cadbeda93dc238c14b57faeb309e6
Tinggxuan <tingxuan_song@mymail.sutd.edu.sg>
2018-04-07 06:54:41 +0000

    normalized funtion to 1 and 0

--- analog/examples/continuous.py
+++ analog/examples/continuous.py
@@ -4,11 +4,11 @@
 import time
 
 # Import the ADS1x15 module.
-import Adafruit_ADS1x15
+import ADS1x15
 
 
 # Create an ADS1115 ADC (16-bit) instance.
-adc = Adafruit_ADS1x15.ADS1115()
+adc = ADS1x15.ADS1115()
 
 # Or create an ADS1015 ADC (12-bit) instance.
 #adc = Adafruit_ADS1x15.ADS1015()
@@ -42,7 +42,7 @@ adc.start_adc(0, gain=GAIN)
 # Read channel 0 for 5 seconds and print out its values.
 print('Reading ADS1x15 channel 0 for 5 seconds...')
 start = time.time()
-while (time.time() - start) <= 5.0:
+while True:
     # Read the last ADC conversion value and print it out.
     value = adc.get_last_result()
     # WARNING! If you try to read any other ADC channel during this continuous
--- analog/examples/simpletest.py
+++ analog/examples/simpletest.py
@@ -9,7 +9,8 @@ import ADS1x15
 
 
 # Create an ADS1115 ADC (16-bit) instance.
-adc = ADS1x15.ADS1115()
+adc1 = ADS1x15.ADS1115(address=0x48)
+adc2 = ADS1x15.ADS1115(address=0x49)
 
 # Or create an ADS1015 ADC (12-bit) instance.
 #adc = Adafruit_ADS1x15.ADS1015()
