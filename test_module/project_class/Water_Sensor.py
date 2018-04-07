2b26e73ea49fd929487d5b0a7c4ceac5e09c5499
TAYTS <taytzushieh@gmail.com>
2018-04-07 23:58:07 +0800

    Update the get_status function for 'Water_Sensor' class to use adc_obj to read the voltage value

--- test_module/project_class/Water_Sensor.py
+++ test_module/project_class/Water_Sensor.py
@@ -6,7 +6,7 @@ class Water_Sensor:
         self.status = self.get_status()
 
     def get_status(self):
-        reading = self.read_adc(self.channel, 1) #1 refer to the GAIN of the sensor to read voltage from 0 to 4.09V
+        reading = self.adc_obj.read_adc(self.channel, 1) #1 refer to the GAIN of the sensor to read voltage from 0 to 4.09V
         result = True if reading > 29000 else False
         retunr result
         
