a4d41ee19470a5c978d842cc991b5b9e83b2808e
TAYTS <taytzushieh@gmail.com>
2018-04-08 10:49:40 +0800

    Create new Tank obj:'rain_buffer_tank' and condition between 'rain_water_tank' and 'rain_buffer_tank'

--- test_module/project_class/main.py
+++ test_module/project_class/main.py
@@ -8,23 +8,32 @@ import Tank
 adc1 = ADS1x15.ADS1115(address=0x48)
 adc2 = ADS1x15.ADS1115(address=0x49)
 
+# initialize the rain_water_tank obj
+rain_water_tank = Tank.Tank({'water_sensor':{'low':0, 'high':1},\
+                             'valve':{'out':14},\
+                             'pump':{'out':15}},\
+                             adc1)
 
-rain_water_tank = Tank.Tank({'water_sensor':{'low':0},\
-                             'valve':{'in':20,'out':21},\
-                             'pump':{'in':19,'out':26}},adc1)
+# initialize the rain_buffer_tank obj
+rain_buffer_tank = Tank.Tank({'water_sensor':{'low':2},\
+                             'valve':{'out':23},\
+                             'pump':{'waster_out':24, 'rain_out':25}},\
+                             adc1)
+
 
 try:
     while True:
-        if rain_water_tank.water_sensor['low'].get_status() == True:
-            rain_water_tank.valve['in'].activate()
-            rain_water_tank.valve['out'].activate()
-            rain_water_tank.pump['in'].activate()
-            rain_water_tank.pump['out'].activate()
-            time.sleep(5)
-            rain_water_tank.valve['in'].deactivate()
-            rain_water_tank.valve['out'].deactivate()
-            rain_water_tank.pump['in'].deactivate()
-            rain_water_tank.pump['out'].deactivate()
+        # condition: rain_water_tank not full and rain_buffer_tank has water
+        if rain_buffer_tank.water_sensor['low'] == True:
+            if rain_water_tank.water_sensor['high'].get_status() == False:
+                rain_buffer_tank.pump['rain_out'].activate()
+            else:
+                rain_buffer_tank.pump['waste_out'].activate()
+        
+        # condition: rain_water_tank full and rain_buffer_tank has water
+        else:
+            rain_buffer_tank.pump['rain_out'].deactivate()
+            rain_buffer_tank.pump['waste_out'].deactivate()
 
 except KeyboardInterrupt:
     GPIO.cleanup()
