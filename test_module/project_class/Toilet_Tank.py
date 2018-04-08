e46679f53f35c90fb23fba1c24bad731fdf781cd
TAYTS <taytzushieh@gmail.com>
2018-04-08 11:49:27 +0800

    - Add new class 'Toilet_Tank' and inherit from class 'Tank' - Add new attribute 'flsuh' to class 'Toilet_Tank'

---
+++ test_module/project_class/Toilet_Tank.py
@@ -0,0 +1,14 @@
+import Tank
+
+class Toilet_Tank(Tank.Tank): # inherit from class 'Tank'
+
+    def __init__(self, kwargs, adc):
+        super().__init__(kwargs,adc): # call the parent class __init__
+        self.flush = flush(adc, kwargs['flush']) # new attributte
+
+
+    def flush(self, adc, channel):
+        # call the parent class water_sensor to initialise the water_sensor obj
+        # use the water sensor as button
+        toilet_flush = super().water_sensor(adc, channel) 
+        return toilet_flush
