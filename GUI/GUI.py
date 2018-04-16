e060dd8081048a826c480b966e256a255edd52a2
TAYTS <taytzushieh@gmail.com>
2018-04-16 22:57:54 +0800

    Add new font 'Gugo-Regular'

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -39,8 +39,9 @@ import threading
 Builder.load_string('''
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 
+# Login Scree and all its child widget
 <LoginScreen>:
-
+    id: loginscreen
     LoginBackgroundImage:
         allow_stretch: True
         keep_ratio: False
@@ -108,6 +109,7 @@ Builder.load_string('''
             LoginButton:
                 username: username
                 password: password
+                loginscreen: loginscreen
 
         LoginMenu:
             pos_hint: {'center_x': 0.5, 'y': 0.1}
@@ -117,7 +119,6 @@ Builder.load_string('''
             canvas:
                 Color:
                     rgb: 1,0,0
-
             QuitButton:
         
 # Define the attribute of the BoxLayout
@@ -175,9 +176,9 @@ Builder.load_string('''
     text: 'LOGIN'
     font_size: self.height*0.6
     padding_y: self.height*0.1
-    font_name: 'Gugi-Regular'#'Archivo_Black'
+    font_name: 'Gugi-Regular'
     background_color: 0,0,0,0
-    on_press: self.login(self.parent, self.parent.pos, self.username, self.password)
+    on_press: self.login(self.parent, self.parent.pos)
     on_release: self.parent.clear_canvas()
 
     canvas.after:
@@ -261,73 +262,10 @@ Builder.load_string('''
 #             on_release:dropdown.select()
        
 
+############################### This is a divider ###########################################
 
-# <SettingsScreen>:
-
-#     SettingBackgroundImage:
-#         allow_stretch: True
-#         keep_ratio: False
-#         opacity: 0.3
-#         source: '.jpeg'
-#         size: self.size
-#         pos: self.pos
-
-#     FloatLayout:
-#         size_hint:0.5,1
-#         pos_hint:{'center_x':0.5,'center_y':0.5}
-        
-#         BoxLayout:
-#             size_hint:1,0.1
-#             pos_hint:{'x':0,'y':0.8}
-#             Lable:
-#                 text:'Control Center'
-#                 font_size:self.height*1
-#                 font_name:'EastSeaDokdo-Regular'
-#                 size_hint:1,1
-#         SettingMenu:
-#             pos_hint: {'x': 0, 'y': 0.6}
-#             SettingTextInput:
-#                 id: Floor_number
-#                 hint_text: 'Floor Number: Choose from 1-10'
-                
-#         SettingMenu:
-#             pos_hint: {'x': 0, 'y': 0.45}
-#             SettingTextInput:
-#                 id: Unit_number
-#                 hint_text: 'Unit Number: Choose from 1-6'
-
-#         SettingMenu:
-#             pos_hint: {'center_x': 0.5, 'y': 0.25}
-#             canvas.before:
-#                 Clear
-#                 Color:
-#                     rgba: 0, 0.902,0.6, self.canvas_opacity #0, 0.902,0.6
-#                 Rectangle:
-#                     size: self.size
-#                     pos: self.pos
-#                 Ellipse:
-#                     pos: self.pos[0] - self.size[1]/2.0, self.pos[1]
-#                     size: self.size[1],self.size[1]
-#                     angle_start: 180
-#                     angle_end: 360
-#                 Ellipse:
-#                     pos: self.size[0] + self.pos[0] - self.size[1]/2.0, self.pos[1]
-#                     size: self.size[1],self.size[1]
-#                     angle_start:360
-#                     angle_end: 540
-#             EnterButton:
-
-#             BackButton:
-                
-#         Label:
-#             text: 'Settings Screen'
-#             font_size: 50
-#         Button:
-#             text: 'Back to menu'
-#             font_size: 50
-#             on_press: 
-#                 root.manager.transition.direction = 'right'
-#                 root.manager.current = 'menu'
+<ControlScreen>:
+    
 ''')
 
 
@@ -369,30 +307,42 @@ class LoginButton(Button):
     # Link widget using ObjectPorperty()
     username = ObjectProperty()
     password = ObjectProperty()
+    loginscreen = ObjectProperty()
 
     # Trigger when login button is pressed
-    def login(self, parent, pos, username, password):
+    def login(self, parent, pos):
 
         # Check database for validation
         parent.show_canvas()
-        self.check_database(username, password)
+        self.check_database()
 
     def remove_background(self, parent):
         parent.clear_canvas()
         
-    def check_database(self, username, password):
-        # print(username.text)
-        if len(username.text.strip()) > 0 or len(password.text.strip())>0:
-            result = usr_database.get("/" + username.text.strip())
+    def check_database(self):
+        if len(self.username.text.strip()) > 0 and len(self.password.text.strip())>0:
+            result = usr_database.get("/" + self.username.text.strip())
 
-            if result == password.text:
+            if result == self.password.text:
                 # Go to control page
-                print("OK, pass validation")
+                self.loginscreen.manager.transition.direction = 'left'
+                self.loginscreen.manager.current = 'controlscreen'
+                
             else:
                 # Prompt the error window and clear the username and password textinput
-                username.text = ""
-                password.text = ""
-    
+                self.username.text = ""
+                self.password.text = ""
+                self.loginscreen.openPopup(1)
+
+        elif len(self.username.text.strip()) > 0:
+            self.loginscreen.openPopup(2)
+
+        elif len(self.password.text.strip()) > 0:
+            self.loginscreen.openPopup(3)
+        else:
+            self.loginscreen.openPopup(4)
+            
+            
 
 class QuitButton(Button):
     
@@ -400,117 +350,51 @@ class QuitButton(Button):
         App.get_running_app().stop()
 
 class LoginScreen(Screen):
-    
-    def enter(self, value):
-        print("enter preesed")
-        print(str(self.t1.text))
-        if self.t1.text in useridpassword:
-            print("done1")
-            if self.t2.text==useridpassword[self.t1.text]:
-                self.manager.transition.direction = 'left'      
-                self.manager.current= 'settings'
-                self.username=self.t1.text
-            else:
-                self.popup.open()
-        else:
-            self.popup.open()
+
+    def openPopup(self, opt):
+        if opt == 1:
+            # Popup for fail login
+            popup = Popup(title='Login Failed', title_size=self.size[0]*0.01,\
+                          content=Label(text='Incorrect username or password.'),\
+                          size_hint=(None, None), size=(self.size[0]*0.3, self.size[0]*0.3),\
+                          on_touchdown=self.dismisspopup)
+            popup.open()
+        
+        elif opt == 2:
+            # Popup for missing password
+            popup = Popup(title='Insufficient Input', title_size=self.size[0]*0.01,\
+                          content=Label(text='Please enter your password.'),\
+                          size_hint=(None, None), size=(self.size[0]*0.3, self.size[0]*0.3),\
+                          on_touchdown=self.dismisspopup)
+            popup.open()
+
+        elif opt == 3:
+            # Popup for missing username
+            popup = Popup(title='Insufficient Input', title_size=self.size[0]*0.01,\
+                          content=Label(text='Please enter your username.'),\
+                          size_hint=(None, None), size=(self.size[0]*0.3, self.size[0]*0.3),\
+                          on_touchdown=self.dismisspopup)
+            popup.open()
+
+        elif opt == 4:
+            # Popup for no info input
+            popup = Popup(title='Insufficient Input', title_size=self.size[0]*0.01,\
+                          content=Label(text='Please enter your username and password.'),\
+                          size_hint=(None, None), size=(self.size[0]*0.3, self.size[0]*0.3),\
+                          on_touchdown=self.dismisspopup)
+            popup.open()
 
     def dismisspopup(self,instance, touch):
-        print("failed login")
         self.popup.dismiss()
 
     def quit_app(self):
         App.get_running_app().stop()
 
 
-class SettingsScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__( **kwargs)
-        self.layout=GridLayout(cols=3,padding=40,spacing=10)
-        with self.canvas:
-    # Add a red color
-            
-            Rectangle(pos=(0,0), size=(900, 600),source="building.jpg")
-            
-        self.add_widget(self.layout)
-        
-        self.blk = ['1','2','3','4','5','6']
-        self.floor = ['1','2','3','4','5','6','7','8','9','10']
-        self.unit = ['1','2','3','4']
-        
-
-#Button(text="[color=000000][b]Progress[/b][/color]", on_press=self.change_to_progress, font_size=70,background_normal = 'growth.jpg', markup = True)
+class ControlScreen(Screen):
        
-        self.l1=Label(text="",valign='bottom',halign='left',font_size=22,font_name='Montserrat-Regular',padding_y=0)
-        with self.l1.canvas:
-            Color(1, 1, 1, 0.3)
-            Rectangle(pos=(0,495), size=(900,200))
+    pass
 
-        self.layout.add_widget(self.l1)
-        l2=Label(text='', valign='bottom',halign='center',font_size=28)
-        self.layout.add_widget(l2)
-        self.but3=Button(text='|  Logout ',on_press=self.change_to_menu,font_size=26,size_hint= (0.25,0.03),background_color=[1, 1, 1,0],font_name='Montserrat-Bold',padding_y=10)
-        self.layout.add_widget(self.but3)
-        
-        self.border10=Label(text="",size_hint= (0.03,0.03))
-        self.layout.add_widget(self.border10)
-        self.border10_2=Label(text="",size_hint= (0.03,0.03))
-        self.layout.add_widget(self.border10_2)
-        self.border10_3=Label(text="",size_hint= (0.03,0.03))
-        self.layout.add_widget(self.border10_3)
-        
-        
-        l2_1=Label(text='', valign='bottom',halign='center',font_size=28)
-        self.layout.add_widget(l2_1)
-        l4=Label(text='Block',valign='bottom',halign='left',font_size=28,padding_x=0,padding_y=0,font_name='Montserrat-Bold')
-        self.layout.add_widget(l4)
-        l2_2=Label(text='', valign='top',halign='left',font_size=28)
-        self.layout.add_widget(l2_2)
-        
-        l6=Label(text='',size_hint= (0.1,0.5))
-        self.layout.add_widget(l6)
-        self.t5=TextInput(hint_text='block number',multiline=False,size_hint= (0.1,0.6),font_name='Montserrat-Regular')
-        self.layout.add_widget(self.t5)
-        l6_2=Label(text='',size_hint= (0.1,0.5))
-        self.layout.add_widget(l6_2)
-        
-        l7_1=Label(text='')
-        self.layout.add_widget(l7_1)
-        l7=Label(text='Floor',valign='bottom',halign='left',font_size=28,padding_x=0,padding_y=0,font_name='Montserrat-Bold')
-        self.layout.add_widget(l7)
-        l7_1=Label(text='')
-        self.layout.add_widget(l7_1)
-        
-        l8_1=Label(text='',size_hint= (0.1,0.5))
-        self.layout.add_widget(l8_1)
-        self.t8=TextInput(hint_text='floor number',multiline=False,size_hint= (0.1,0.6),font_name='Montserrat-Regular')
-        self.layout.add_widget(self.t8)
-        l8_1=Label(text='',size_hint= (0.1,0.5))
-        self.layout.add_widget(l8_1)
-        
-        l9=Label(text='')
-        self.layout.add_widget(l9)
-        self.border9_1=Label(text="")
-        self.layout.add_widget(self.border9_1)
-        self.border9_2=Label(text="")
-        self.layout.add_widget(self.border9_2)
-        self.border9_3=Label(text="",size_hint= (0.2,0.5))
-        self.layout.add_widget(self.border9_3)
-        
-        b12=Button(text='Enter',font_size=24,size_hint= (0.2,0.5),background_color=[1, 154, 1, 1],on_press=self.change_to_toggle,font_name='Montserrat-Bold')
-        self.layout.add_widget(b12)
-        self.border6_1=Label(text="",size_hint= (0.2,0.5))
-        self.layout.add_widget(self.border6_1)
-        self.border6_2=Label(text="")
-        self.layout.add_widget(self.border6_2)
-        self.border6_3=Label(text="")
-        self.layout.add_widget(self.border6_3)
-        self.border6_4=Label(text="")
-        self.layout.add_widget(self.border6_4)
-        
-        
-    
-        
     def change_to_toggle(self,value):
         print("enter2 pressed")
                     
@@ -639,14 +523,14 @@ class Toggle(Screen):
 
 class SwitchScreenApp(App):
     def build(self):
-            sm=ScreenManager()
-            loginscreen=LoginScreen(name='login')
-            st=SettingsScreen(name='settings')
-            tgl=Toggle(name="toggle")
-#            err=SettingsScreen(name="error")
-#            sm.add_widget(err)
+            sm = ScreenManager()
+            loginscreen = LoginScreen(name='login')
+            controlscreen = ControlScreen(name='controlscreen')
+            # tgl=Toggle(name="toggle")
+            # err=SettingsScreen(name="error")
+            # sm.add_widget(err)
             sm.add_widget(loginscreen)
-            # sm.add_widget(st)
+            sm.add_widget(controlscreen)
             # sm.add_widget(tgl)
             sm.current='login'
             return sm
